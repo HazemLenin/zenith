bb27e3a271e484bc69338389071bf31e7d293ed2
Hala saber <halasabermhamed11@gmail.com>
2025-05-07 20:05:42 +0300

    "add Card"

---
+++ zenith-frontend/src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = ({ children }) => {
+  return (
+    <div className="flex items-center justify-between w-full  bg-white border-l-4 border-[#2a5c8a] rounded-md shadow-md hover:shadow-lg transition-all duration-300 p-4">
+      <div className="flex flex-col justify-center pr-4">{children}</div>
+    </div>
+  );
+};
+
+export default Card;
